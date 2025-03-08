570a8a84dbd2e663ef9048dc1a2e6b48c56cabbb
Aryan Kaul <Aryankaul2001@gmail.com>
2025-03-08 16:56:20 +0530

    add local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { SpinWheel } from 'react-spin-wheel'
+import 'react-spin-wheel/dist/index.css'
 
 function App() {
   const [choices, setChoices] = useState<string[]>([])
   const [currentChoice, setCurrentChoice] = useState<string>('')
   const [hasDuplicate, setHasDuplicate] = useState<boolean>(false)
+
+  // Load choices from local storage on initial render
+  useEffect(() => {
+    const storedChoices = localStorage.getItem('choices')
+    if (storedChoices) {
+      setChoices(JSON.parse(storedChoices))
+    }
+  }, [])
 
   const handleAddChoice = () => {
     if (choices.find((choice) => choice === currentChoice)) {
@@ -13,12 +23,25 @@ function App() {
       }, 2000)
       return
     }
-    setChoices([...choices, currentChoice])
+    const newChoices = [...choices, currentChoice]
+    setChoices(newChoices)
+    localStorage.setItem('choices', JSON.stringify(newChoices))
     setCurrentChoice('')
   }
 
+  const handleClearChoices = () => {
+    setChoices([])
+    localStorage.removeItem('choices')
+  }
+
+  const handleRemoveChoice = (index: number) => {
+    const newChoices = choices.filter((_, i) => i !== index)
+    setChoices(newChoices)
+    localStorage.setItem('choices', JSON.stringify(newChoices))
+  }
+
   return (
-    <div className='flex justify-center w-dvw h-dvh py-8'>
+    <div className='flex justify-center w-dvw min-h-dvh h-full py-8'>
       <div className='max-w-3xl w-full flex flex-col items-center gap-10'>
         <h1 className='text-4xl font-bold text-center font-[arial]'>Study Roulette</h1>
 
@@ -31,12 +54,12 @@ function App() {
         </p>
 
         {/* input logic */}
-        <div className='flex flex-col gap-10'>
-          <div className='flex flex-row items-center justify-between gap-10 '>
+        <div className='flex flex-col gap-10 w-full'>
+          <div className='flex flex-row items-center justify-between gap-10 w-full'>
             <input
               type='text'
               placeholder='Enter your choices here'
-              className='w-96 p-2 border border-gray-300 rounded focus:outline-none'
+              className='p-2 border border-gray-300 rounded focus:outline-none w-full'
               value={currentChoice}
               onChange={(e) => {
                 setHasDuplicate(false)
@@ -49,7 +72,7 @@ function App() {
               }}
             />
             <button
-              className='bg-gray-700 text-white p-2 rounded disabled:bg-gray-500 disabled:cursor-not-allowed cursor-pointer'
+              className='bg-gray-700 text-white text-nowrap p-2 rounded disabled:bg-gray-500 disabled:cursor-not-allowed cursor-pointer'
               disabled={currentChoice === ''}
               onClick={handleAddChoice}
             >
@@ -67,7 +90,7 @@ function App() {
             <div className='flex justify-between items-center'>
               <span className='text-lg'>Your Choices:</span>
               <button
-                onClick={() => setChoices([])}
+                onClick={handleClearChoices}
                 className={`bg-red-500 text-white px-2 rounded disabled:bg-gray-700 disabled:cursor-not-allowed cursor-pointer text-sm hover:bg-red-700 transition-colors duration-300`}
               >
                 Clear
@@ -78,14 +101,31 @@ function App() {
                 <button
                   key={index + choice}
                   className='bg-gray-600 px-2 rounded cursor-pointer'
-                  onClick={() => setChoices(choices.filter((_, i) => i !== index))}
+                  onClick={() => {
+                    handleRemoveChoice(index)
+                  }}
                 >
                   {choice}
                 </button>
               ))}
             </div>
           </div>
+          {choices.length < 2 && (
+            <p className='text-red-500'>Please enter at least 2 choices to spin the wheel.</p>
+          )}
         </div>
+        {/* spinner */}
+        {choices.length >= 2 && (
+          <div className=''>
+            <SpinWheel
+              items={choices}
+              onFinishSpin={(item) => {
+                alert(`You should study: ${String(item)}`)
+              }}
+              borderColor='gray'
+            />
+          </div>
+        )}
       </div>
     </div>
   )
